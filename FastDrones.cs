cc0842e8a4815ab943fa4bc258e16af6f40b3052
David Koppstein <david.koppstein@gmail.com>
2021-03-31 23:33:26 +0200

    update to 0.0.5, increase speed limit to 299

--- FastDrones.cs
+++ FastDrones.cs
@@ -5,15 +5,15 @@ using System;
 using System.Collections.Generic;
 using System.Text;
 
-
 namespace com.dkoppstein.plugin.DSP.FastDrones
 {
-    [BepInPlugin("com.dkoppstein.plugin.DSP.FastDrones", "FastDrones", "0.0.4")]
+    [BepInPlugin("com.dkoppstein.plugin.DSP.FastDrones", "FastDrones", "0.0.5")]
     [BepInProcess("DSPGAME.exe")]
     public class FastDronesPlugin : BaseUnityPlugin
     {
-        private static readonly float DRONE_SPEED = 59.0F; // necessary until bug is fixed capping at 60
-        private static readonly double DECREASE_DRONE_ENERGY_FACTOR = 50.0;
+        private static readonly float DRONE_SPEED = 299.0F; // max of MAX_DIST_DELTA / min(dt) (probably 0.01)
+        private static readonly double DECREASE_DRONE_ENERGY_FACTOR = 50.0; 
+        private static readonly float MAX_DIST_DELTA = 3f; // minimum distance from drone to player/target before state changes
 
         private void Start()
         {
@@ -31,12 +31,9 @@ namespace com.dkoppstein.plugin.DSP.FastDrones
         {
             [HarmonyPrefix]
             [HarmonyPatch("Update")]
-            public static void ChangeDroneSpeedPrefix(ref MechaDrone __instance,
-                                                      PrebuildData[] prebuildPool,
+            public static bool ChangeDroneSpeedPrefix(ref MechaDrone __instance,
                                                       Vector3 playerPos,
-                                                      float dt,
-                                                      ref double energy,
-                                                      ref double energyChange,
+                                                      ref int __result,
                                                       ref double energyRate, 
                                                       out float __state)
             {
@@ -47,21 +44,39 @@ namespace com.dkoppstein.plugin.DSP.FastDrones
                 energyRate = (double)energyRate / (double)FastDronesPlugin.DECREASE_DRONE_ENERGY_FACTOR;
                 //FileLog.Log(string.Format("New drone speed after prefix is: {0}", (object)__instance.speed));
                 //FileLog.Log(string.Format("New energy rate after prefix is: {0}", (object) energyRate));
+
+                // thanks to Appun for the following code and figuring out 3f
+                Vector3 toTargetVector = new Vector3(__instance.target.x - __instance.position.x, __instance.target.y - __instance.position.y, __instance.target.z - __instance.position.z);
+                float distToTarget = (float)Math.Sqrt((double)(toTargetVector.x * toTargetVector.x + toTargetVector.y * toTargetVector.y + toTargetVector.z * toTargetVector.z));
+
+                if (__instance.stage == 2 && distToTarget < FastDronesPlugin.MAX_DIST_DELTA) // drone is building something and near target
+                    {
+                        __result = 1; // build the thing
+                        return false;
+                    }
+
+                Vector3 toPlayerVector = new Vector3(playerPos.x - __instance.position.x, playerPos.y - __instance.position.y, playerPos.z - __instance.position.z);
+                float distToPlayer = (float)Math.Sqrt((double)(toPlayerVector.x * toPlayerVector.x + toPlayerVector.y * toPlayerVector.y + toPlayerVector.z * toPlayerVector.z));
+
+                if (__instance.stage == 3 && distToPlayer < FastDronesPlugin.MAX_DIST_DELTA) // drone is coming home and near home
+                {
+                    __instance.Reset(); 
+                    __result = 0; 
+                    return false;
+                }
+
+                return true;
             }
+
+
             [HarmonyPostfix]
             [HarmonyPatch("Update")]
             public static void ChangeDroneSpeedPostfix(ref MechaDrone __instance,
-                                                      PrebuildData[] prebuildPool,
-                                                      Vector3 playerPos,
-                                                      float dt,
-                                                      ref double energy,
-                                                      ref double energyChange,
-                                                      ref double energyRate,
                                                       float __state)
             {
                 //FileLog.Log(string.Format("Drone speed before postfix is: {0}", (object)__instance.speed));
                 //FileLog.Log(string.Format("Energy rate before postfix is: {0}", (object) energyRate));
-                __instance.speed = __state; // reset back to original
+                __instance.speed = __state; // reset speed back to original so as not to interfere with objects
                 //FileLog.Log(string.Format("New drone speed after postfix is: {0}", (object)__instance.speed));
                 //FileLog.Log(string.Format("New energy rate after postfix is: {0}", (object) energyRate));
             }
--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # DSP_FastDrones
-This mod increases the speed of Mecha Drones to 59.0 (capped at 59.0 until developers enable faster speeds). It also decreases energy usage of drones by a set factor (default: 50.0).
+This mod increases the speed of Mecha Drones to 299 (previous max speed was 59). It also decreases energy usage of drones by a set factor (default: 50.0). 
+The mod does not require a new game start, it modifies the speed on the fly, and will not affect your savefile, so you can go back to old speeds if you want.  
 
-The mod does not require a new game start, it modifies the speed on the fly. 
+Many thanks to @Appun for figuring out how to break the speed limit and for suggestions/code. 
